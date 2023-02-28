2e0350dd1bcab34f82fccd2f8d1d31671e5161e5
Taylor Ambrose <taykesby@gmail.com>
2023-02-28 13:22:09 -0500

    created attempt1 branch

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+//Attempt 1
+
 function App() {
   return (
     <div className="app">
